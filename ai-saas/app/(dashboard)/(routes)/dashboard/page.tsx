4ba5b9eeb3c2486d2925b8e323b459dfeffc98ca
SebastianEscudero <sebastin.escudero@uc.cl>
2023-12-26 17:17:03 -0300

    sidebar and navbar also clerk implentation

---
+++ ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+const DashboardPage = () => {
+    return ( 
+        <div>
+            <p>
+                Dashboard Page (Protected)
+            </p>
+        </div>
+     );
+}
+ 
+export default DashboardPage;
