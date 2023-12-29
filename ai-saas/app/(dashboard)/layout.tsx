a212e51245a4ba9ac34900bcf36d6b79393f80aa
SebastianEscudero <sebastin.escudero@uc.cl>
2023-12-28 21:19:29 -0300

    upgrade on the bottom of the page

--- ai-saas/app/(dashboard)/layout.tsx
+++ ai-saas/app/(dashboard)/layout.tsx
@@ -11,10 +11,8 @@ const DashboardLayout = async ({
 
     return ( 
         <div className="h-full relative">
-            <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0  bg-gray-900">
-                <div>
-                    <Sidebar apiLimitCount={apiLimitCount}/>
-                </div>
+            <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
+                <Sidebar apiLimitCount={apiLimitCount}/>
             </div>
             <main className="md:pl-72 pb-10">
                 <Navbar />
--- ai-saas/components/sidebar.tsx
+++ ai-saas/components/sidebar.tsx
@@ -70,7 +70,7 @@ const Sidebar = ({
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
                 <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-                    <div className="relative w-8 h-8 mr-4">
+                    <div className="relative h-8 w-8 mr-4">
                         <Image 
                             fill
                             alt="Logo"
@@ -86,8 +86,10 @@ const Sidebar = ({
                         <Link
                             href={route.href}
                             key={route.href}
-                            className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
-                            pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}
+                            className={cn(
+                                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                                pathname === route.href ? "text-white bg-white/10" : "text-zinc-400",
+                            )}
                         >
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("h-5 w-5 mr-3", route.color)}/>
